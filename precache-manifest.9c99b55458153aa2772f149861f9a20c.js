36142bffd3b5f69249d263507f57bb3face5b8b1
Alexander <alexanderespinozajesus@gmail.com>
2020-10-09 17:33:31 -0500

    Updates

---
+++ precache-manifest.9c99b55458153aa2772f149861f9a20c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e6f9586d4cf5d4de75a36e90e4daf3",
+    "url": "/react-two/index.html"
+  },
+  {
+    "revision": "801ef260f6f0f5db8a11",
+    "url": "/react-two/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23ed7e81d4b3825a5da1",
+    "url": "/react-two/static/js/2.cd579fa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-two/static/js/2.cd579fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801ef260f6f0f5db8a11",
+    "url": "/react-two/static/js/main.28332bfd.chunk.js"
+  },
+  {
+    "revision": "ad9523e32dc9fc634519",
+    "url": "/react-two/static/js/runtime-main.c274b9ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-two/static/media/logo.5d5d9eef.svg"
+  }
+]);
